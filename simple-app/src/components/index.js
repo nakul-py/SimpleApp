398590f9abd02fd4dd2d802ee5ad8993b1a3543f
Nakul <nakulverma.py@gmail.com>
2025-05-22 22:00:55 +0530

    add new components to index export: Button, Input, PostCard, Login, SignUp, and ProtectedContainer

--- simple-app/src/components/index.js
+++ simple-app/src/components/index.js
@@ -3,5 +3,12 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ProtectedContainer from "./AuthLayout";
+import Select from "./Select";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button , Input, PostCard, Login, SignUp, ProtectedContainer, Select };
