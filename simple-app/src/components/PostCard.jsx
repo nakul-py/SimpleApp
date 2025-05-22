25760ddb4d5b320bc4e6a0b82b09cc0ad70aaacf
Nakul <nakulverma.py@gmail.com>
2025-05-22 21:58:20 +0530

    add PostCard and Select components for displaying posts and selecting options

---
+++ simple-app/src/components/PostCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import service from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+function Postcard({ $id, title, featuredImage }) {
+  const imageUrl = featuredImage? service.getFilePreview(featuredImage) : null
+
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-purple-400 px-4 rounded-xl">
+        <div className="w-full justify-center mb-4"></div>
+        <img src={imageUrl}
+         alt={title}
+         className="w-full h-64 object-cover rounded-xl" /> 
+      </div>
+      <h2 className=" font-bold text-xl">{title}</h2>
+    </Link>
+  );
+}
+
+export default Postcard;
