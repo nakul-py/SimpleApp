70042f99697094ac6743709df39f9f6097c1c146
Nakul <nakulverma.py@gmail.com>
2025-05-24 21:16:20 +0530

    refactor: rename ProtectedContainer import to AuthLayout for consistency

--- simple-app/src/components/index.js
+++ simple-app/src/components/index.js
@@ -8,9 +8,9 @@ import Input from "./Input";
 import PostCard from "./PostCard";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import ProtectedContainer from "./AuthLayout";
+import AuthLayout from "./AuthLayout";
 import Select from "./Select";
 import RTE from "./RTE";
 import PostForm from "./post-form/Postform";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button , Input, PostCard, Login, SignUp, ProtectedContainer, Select , RTE, PostForm };
+export { Header, Footer, Container, Logo, LogoutBtn, Button , Input, PostCard, Login, SignUp, AuthLayout, Select , RTE, PostForm };
