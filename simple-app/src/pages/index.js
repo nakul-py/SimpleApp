1accfc6145f62a2c5c505e2ec416d90f849341a3
Nakul <nakulverma.py@gmail.com>
2025-05-25 20:54:49 +0530

    feat: add Home page component for displaying posts and authentication handling

---
+++ simple-app/src/pages/index.js
@@ -0,0 +1,9 @@
+import Addposts from "./Addposts"
+import Allposts from "./Allposts";
+import Editposts from "./Editposts";
+import Home from "./Home";
+import Login from "./Login";
+import Posts from "./Posts";
+import SignUp from "./SignUp";
+
+export { Addposts, Allposts, Editposts, Home, Login, Posts, SignUp };
