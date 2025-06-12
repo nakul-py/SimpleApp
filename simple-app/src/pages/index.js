fe4f079883726be4b53a827645de9b1c4b4c5934
Nakul <nakulverma.py@gmail.com>
2025-06-12 22:30:28 +0530

    refactor: remove unused components and pages related to authentication and post management

--- simple-app/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Addposts from "./Addposts"
-import Allposts from "./Allposts";
-import Editposts from "./Editposts";
-import Home from "./Home";
-import Login from "./Login";
-import Posts from "./Posts";
-import SignUp from "./SignUp";
-
-export { Addposts, Allposts, Editposts, Home, Login, Posts, SignUp };
