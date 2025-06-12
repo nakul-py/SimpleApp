6ea0c26dea93d7d62559601edfea862f12143357
Nakul <nakulverma.py@gmail.com>
2025-06-12 22:30:54 +0530

    refactor: remove unused styles, components, and configuration files related to Tailwind CSS and Appwrite

--- simple-app/src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-
-function Logo({ width = '100px', rounded = false }) {
-    return <img src={logo} alt="Logo" style={{ width, borderRadius: rounded ? '50%' : '450%' }} />
-}
-
-export default Logo
